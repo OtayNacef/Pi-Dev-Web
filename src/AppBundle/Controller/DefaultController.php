97991d715f2fd3ff6d238b84703057b99b9cc0b4
Ala <alaeddin.tabai@esprit.tn>
2019-01-30 05:41:32 -0800

    template2

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,18 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+//        // replace this example code with whatever you need
+//        return $this->render('default/index.html.twig', [
+//            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+//        ]);
+
+        /** @var  \FOS\UserBundle\Form\Factory\FactoryInterface $formFactory*/
+        $formFactory = $this->container->get('fos_user.registration.form.factory');
+
+        $form = $formFactory->createForm();
+
+        return $this->container->get('templating')->renderResponse('default/index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
