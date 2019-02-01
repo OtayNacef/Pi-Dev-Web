2ef9cf650eb00ff781ef70d51dce2d705a437699
Otay Nacef <nacef.otay@esprit.tn>
2019-02-01 01:41:33 +0100

    Add Album and Status

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -5,10 +5,14 @@ namespace UserBundle\Controller;
 use FOS\UserBundle\Event\GetResponseUserEvent;
 use FOS\UserBundle\FOSUserEvents;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
+use UserBundle\Entity\Album;
+use UserBundle\Entity\CentreInteret;
 use UserBundle\Entity\Publication;
 use UserBundle\Entity\User;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class DefaultController extends Controller
 {
@@ -23,7 +27,7 @@ class DefaultController extends Controller
                 $p= $em->getRepository(Publication::class)->find($request->get("idpubd"));
                 $em->remove($p);
                 $em->flush();
-                return $this->redirectToRoute("profil_homepage");
+                return $this->redirectToRoute("user_profil");
             }
             if ($request->request->has('idpubmodal')) {
                 $p= $em->getRepository(Publication::class)->find($request->get("idpubmodal"));
@@ -32,7 +36,7 @@ class DefaultController extends Controller
                 $p->setDatePublication($d);
                 $em->persist($p);
                 $em->flush();
-                return $this->redirectToRoute("profil_homepage");
+                return $this->redirectToRoute("user_profil");
             }
             if ($request->request->has('contenuajout')) {
                 $p = new Publication();
@@ -43,9 +47,9 @@ class DefaultController extends Controller
                 $em->persist($p);
                 $em->flush();
             }
-            return $this->redirectToRoute('profil_homepage');
+            return $this->redirectToRoute('user_profil');
         }
-        return $this->render('@User/profil.html.twig', array(
+        return $this->render('@User/Backprofil.html.twig', array(
             'iduser' => $u->getId(),'curr_user' => $u,'pubs'=>$pubs
         ));
 
@@ -96,6 +100,57 @@ class DefaultController extends Controller
 
         return $this->render('@User/parameter.html.twig', array(
             'iduser' => $u->getId(), 'us' => $u, 'form' => $form->createView()
+        ));
+    }
+
+    public function descriptionAction()
+    {
+        $u= $this->container->get('security.token_storage')->getToken()->getUser();
+        $em = $this->getDoctrine()->getManager();
+
+        $films = $em->getRepository(CentreInteret::class)->findBy(array('user' => $u->getId(),'type' => 'film'));
+        $series = $em->getRepository(CentreInteret::class)->findBy(array('user' => $u->getId(),'type' => 'serie'));
+        $artists = $em->getRepository(CentreInteret::class)->findBy(array('user' => $u->getId(),'type' => 'artist'));
+        $livres = $em->getRepository(CentreInteret::class)->findBy(array('user' => $u->getId(),'type' => 'livre'));
+        return $this->render('@User/description.html.twig', array(
+            'iduser' => $u->getId(),'curr_user' => $u,'films'=>$films,'series'=>$series,'artists'=>$artists,'livres'=>$livres
+        ));
+    }
+    public function albumAction(Request $request)
+    {
+        $u= $this->container->get('security.token_storage')->getToken()->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $album = new Album();
+        //----------------
+        $form=$this->createFormBuilder($album)
+            ->add('imageFile', VichImageType::class)
+            ->add('user', HiddenType::class, array('data' => $u))
+            ->add('Ajouter',SubmitType::class)
+            ->getForm();
+        $form->handleRequest($request);
+        if (($form->isSubmitted())&&($form->isValid()))
+        {
+            $album=$form->getData();
+            $album->setUser($u);
+            $em->persist($album);
+            $em->flush();
+            return $this->redirectToRoute('user_album');
+        }
+        //-------------------supprimer photo
+        if ($request->isMethod('POST')) {
+            if ($request->request->has('idp')) {
+                $p= $em->getRepository(Album::class)->find($request->get("idp"));
+                $em->remove($p);
+                $em->flush();
+                return $this->redirectToRoute("user_album");
+            }
+            return $this->redirectToRoute('user_album');
+        }
+        //----------------------------------
+        $photos=$em->getRepository(Album::class)->findBy(array('user' => $u->getId()),array('datePublication' => 'ASC'));
+
+        return $this->render('@User/album.html.twig', array(
+            'curr_user' => $u,'form'=>$form->createView(),'photos'=>$photos
         ));
     }
 
