8bc72825a684c36fa026d92445c44534fb726e0d
mariem <mariem.smari@esprit.tn>
2019-02-07 16:11:30 +0100

    first commit

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,7 +14,7 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
 //        // replace this example code with whatever you need
-//        return $this->render('default/index.html.twig', [
+//        return $this->render('default/choixbonplan.html.twig', [
 //            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
 //        ]);
 
@@ -23,7 +23,7 @@ class DefaultController extends Controller
 
         $form = $formFactory->createForm();
 
-        return $this->container->get('templating')->renderResponse('default/index.html.twig', array(
+        return $this->container->get('templating')->renderResponse('default/choixbonplan.html.twig', array(
             'form' => $form->createView(),
         ));
     }
