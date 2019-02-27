b3717619b2d26983095231b469037a2c3c1be384
Hsine <hsine.gabsi@esprit.tn>
2019-02-27 14:31:10 +0100

    publication groupe

---
+++ src/GroupBundle/Form/SignalgroupType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace GroupBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SignalgroupType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('cause');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'GroupBundle\Entity\Signalgroup'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'groupbundle_signalgroup';
+    }
+
+
+}
